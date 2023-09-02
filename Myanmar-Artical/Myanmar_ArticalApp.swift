88efc0ea9f8a17b1eb985f0008cc33f2e29cf148
Kelvin Gao <u6511753@au.edu>
2023-09-02 11:34:47 +0700

    Initial Commit

---
+++ Myanmar-Artical/Myanmar_ArticalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Myanmar_ArticalApp.swift
+//  Myanmar-Artical
+//
+//  Created by Kelvin Gao  on 2/9/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct Myanmar_ArticalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
