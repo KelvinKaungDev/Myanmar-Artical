15d9ea1b7343745b0f4b6f6888cc0104659d04ea
Kelvin Kaung <107702120+KelvinKaungDev@users.noreply.github.com>
2024-06-28 10:20:38 +0700

    first commit

---
+++ Myanmar-Artical/MenuView.swift
@@ -0,0 +1,20 @@
+//
+//  MenuView.swift
+//  Myanmar-Artical
+//
+//  Created by Kelvin Gao  on 2/9/2566 BE.
+//
+
+import SwiftUI
+
+struct MenuView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MenuView_Previews: PreviewProvider {
+    static var previews: some View {
+        MenuView()
+    }
+}
